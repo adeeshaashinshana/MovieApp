d3bef3590f796ca87ae60d88bc8729caa3b52f80
adeeshaashinshana <69760799+adeeshaashinshana@users.noreply.github.com>
2021-08-10 18:44:18 +0530

    basic card

---
+++ movie-app/src/api/movieInfo.js
@@ -0,0 +1,52 @@
+const movieInfo = [
+    {
+      price: "Just $1 per user",
+      priceSub: "per month",
+      timeLimit: "(1 - 100 users)",
+      planName: "Starter Plan",
+      features: [
+        '"Matti" Engagement Bot',
+        "Find Colleagues",
+        "Goal Tracking",
+        "Badge Features"
+      ]
+    },
+    {
+      price: "$2 per user",
+      priceSub: "per month",
+      timeLimit: "(100+ users)",
+      planName: "Pro Plan",
+      features: [
+        '"Matti" Engagement Bot',
+        "Find Colleagues",
+        "Goal Tracking",
+        "Badge Features"
+      ]
+    },
+    {
+      price: "$2 per user",
+      priceSub: "per month",
+      timeLimit: "(100+ users)",
+      planName: "Pro Plan",
+      features: [
+        '"Matti" Engagement Bot',
+        "Find Colleagues",
+        "Goal Tracking",
+        "Badge Features"
+      ]
+    },
+    {
+      price: "$2 per user",
+      priceSub: "per month",
+      timeLimit: "(100+ users)",
+      planName: "Pro Plan",
+      features: [
+        '"Matti" Engagement Bot',
+        "Find Colleagues",
+        "Goal Tracking",
+        "Badge Features"
+      ]
+    }
+  ];
+  export default movieInfo;
+  
--- movie-app/src/container/cardHolder/CardHolder.js
+++ movie-app/src/container/cardHolder/CardHolder.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import { Form, FormControl, Button } from 'react-bootstrap';
+import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
 import MovieCard from '../../component/movieCard/MovieCard';
+import movieInfo from '../../api/movieInfo';
 import './CardHolder.css';
 
 const CardHolder = () => {
+
+    const renderMovieInfoCard = movieInfo.map((details, i) => (
+        <Col lg="4" key={i}>
+            <MovieCard
+                price={details.price}
+                priceSub={details.priceSub}
+                timeLimit={details.timeLimit}
+                planName={details.planName}
+                feats={details.features.map(details => {
+                    return <Col> {details} </Col>;
+                })}
+                onBuyClicked={() => null}
+            />
+        </Col>
+    ));
+
     return (
         <div className="CardHolderContainer">
-            <MovieCard/>
+            <Container>
+                <Row>
+                    <Col lg="12">
+                        <h1 className="title">Make a better workplace today!</h1>
+                        <p className="sub-title">
+                            Choose a package which best suites your organization
+                        </p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col lg="2"></Col>
+                    {renderMovieInfoCard}
+                    <Col lg="2"></Col>
+                </Row>
+            </Container>
         </div>
     );
 }
